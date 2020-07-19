bfb11c968093faeecbfc0f1cf092a5d13f9b5bd1
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-19 14:19:03 +0530

    Add css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import "./style.css"
 console.log('testing')
+
+
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = 'Jaspreet singh';
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
