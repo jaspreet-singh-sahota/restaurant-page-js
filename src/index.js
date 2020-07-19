ce73e0c67d3bbc5e328035b1d7c14f30d6fb2916
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-20 01:57:44 +0530

    Add menu page boiler plate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import "./styles/style.css"
 import background from './js/background';
 import navbar from './js/navbar';
-import home from './js/home';
+// import home from './js/home';
 
 const content = document.getElementById('content');
 content.appendChild(background);
 content.appendChild(navbar);
-content.appendChild(home);
+// content.appendChild(home);
