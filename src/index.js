42a821c7eb232abeffe7ce9eb92c78462e3e4a7c
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-19 17:08:14 +0530

    Add home-page boiler plate

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ console.log('testing')
 
 
 function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = 'Jaspreet singh';
-  element.classList.add('hello');
-
-  return element;
+ 
 }
 
 document.body.appendChild(component());
