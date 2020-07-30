a8874082742a1ceb49b3636d49b8320bac0c288d
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-30 22:18:26 +0530

    Fix linter errors

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,9 @@
 const path = require('path');
+/* eslint-disable */
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+/* eslint-enable */
 
 module.exports = {
   entry: {
